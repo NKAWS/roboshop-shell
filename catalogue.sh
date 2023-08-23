6bf434e1b6d08d676916f5e9378b5c00c99fab40
Naresh kumar <naresh.kesa19@gmail.com>
2023-08-23 02:47:58 -0700

    commomon.sh

--- catalogue.sh
+++ catalogue.sh
@@ -1,32 +1,4 @@
-log=/tmp/roboshop.log
-   echo -e "\e[36m<<<<<<<<<<<<<<<< Create Catalogue Service >>>>>>>>>>>>>>\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service &>>${log}
-   echo -e "\e[36m<<<<<<<<<<<<<<<< Create Mongodb Repo >>>>>>>>>>>>>>\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repoec &>>${log}
-   echo -e "\e[36m<<<<<<<<<<<<<<<< Install nodejs Repos >>>>>>>>>>>>>>\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
-   echo -e "\e[36m<<<<<<<<<<<<<<<< Install NodeJS >>>>>>>>>>>>>>\e[0m"
-yum install nodejs -y &>>${log}
-   echo -e "\e[36m<<<<<<<<<<<<<<<< Create Application User >>>>>>>>>>>>>>\e[0m"
-useradd roboshop &>>${log}
-   echo -e "\e[36m<<<<<<<<<<<<<<<< Cleaning the content >>>>>>>>>>>>>>\e[0m"
-rm -rf /app &>>${log}
-   echo -e "\e[36m<<<<<<<<<<<<<<<< Create Application Directory >>>>>>>>>>>>>>\e[0m"
-mkdir /app &>>${log}
-   echo -e "\e[36m<<<<<<<<<<<<<<<< Download Application Content >>>>>>>>>>>>>>\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
-    echo -e "\e[36m<<<<<<<<<<<<<<<< Extraction Application Content >>>>>>>>>>>>>>\e[0m"
-cd /app
-unzip /tmp/catalogue.zip &>>${log}
-cd /app
-    echo -e "\e[36m<<<<<<<<<<<<<<<< Download nodeJS dependencies >>>>>>>>>>>>>>\e[0m"
-npm install &>>${log}
-    echo -e "\e[36m<<<<<<<<<<<<<<<<Install Mongo client >>>>>>>>>>>>>>\e[0m"
-yum install mongodb-org-shell -y &>>${log}
-    echo -e "\e[36m<<<<<<<<<<<<<<<< Load Catalogue schema >>>>>>>>>>>>>>\e[0m"
-mongo --host mongodb.nkdevops74.online </app/schema/catalogue.js &>>${log}
+component=catalogue
+source common.sh
 
-    echo -e "\e[36m<<<<<<<<<<<<<<<< Start Catalogue service >>>>>>>>>>>>>>\e[0m"
-systemctl daemon-reload &>>${log}
-systemctl enable catalogue &>>${log}
-systemctl restart catalogue &>>${log}
+nodejs
---
+++ common.sh
@@ -0,0 +1,35 @@
+nodejs() {
+  log=/tmp/roboshop.log
+     echo -e "\e[36m<<<<<<<<<<<<<<<< Create User Service >>>>>>>>>>>>>>\e[0m"
+  cp catalogue.service /etc/systemd/system/${component}.service &>>${log}
+     echo -e "\e[36m<<<<<<<<<<<<<<<< Create Mongodb Repo >>>>>>>>>>>>>>\e[0m"
+  cp mongo.repo /etc/yum.repos.d/mongo.repoec &>>${log}
+     echo -e "\e[36m<<<<<<<<<<<<<<<< Install nodejs Repos >>>>>>>>>>>>>>\e[0m"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+     echo -e "\e[36m<<<<<<<<<<<<<<<< Install NodeJS >>>>>>>>>>>>>>\e[0m"
+  yum install nodejs -y &>>${log}
+     echo -e "\e[36m<<<<<<<<<<<<<<<< Create Application User >>>>>>>>>>>>>>\e[0m"
+  useradd roboshop &>>${log}
+     echo -e "\e[36m<<<<<<<<<<<<<<<< Cleaning the content >>>>>>>>>>>>>>\e[0m"
+  rm -rf /app &>>${log}
+     echo -e "\e[36m<<<<<<<<<<<<<<<< Create Application Directory >>>>>>>>>>>>>>\e[0m"
+  mkdir /app &>>${log}
+     echo -e "\e[36m<<<<<<<<<<<<<<<< Download Application Content >>>>>>>>>>>>>>\e[0m"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+      echo -e "\e[36m<<<<<<<<<<<<<<<< Extraction Application Content >>>>>>>>>>>>>>\e[0m"
+  cd /app
+  unzip /tmp/${component}.zip &>>${log}
+  cd /app
+      echo -e "\e[36m<<<<<<<<<<<<<<<< Download nodeJS dependencies >>>>>>>>>>>>>>\e[0m"
+  npm install &>>${log}
+      echo -e "\e[36m<<<<<<<<<<<<<<<<Install Mongo client >>>>>>>>>>>>>>\e[0m"
+  yum install mongodb-org-shell -y &>>${log}
+      echo -e "\e[36m<<<<<<<<<<<<<<<< Load Catalogue schema >>>>>>>>>>>>>>\e[0m"
+  mongo --host mongodb.nkdevops74.online </app/schema/${component}.js &>>${log}
+
+      echo -e "\e[36m<<<<<<<<<<<<<<<< Start Catalogue service >>>>>>>>>>>>>>\e[0m"
+  systemctl daemon-reload &>>${log}
+  systemctl enable ${component} &>>${log}
+  systemctl restart ${component} &>>${log}
+
+}
