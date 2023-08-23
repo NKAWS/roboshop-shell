2473c4309eddd1b3ee8b5b4ff7ec6966ed9f3d9c
Naresh kumar <naresh.kesa19@gmail.com>
2023-08-23 00:16:14 -0700

    Shell Scripting added

--- catalogue.sh
+++ catalogue.sh
@@ -1,29 +1,29 @@
-   echo"<<<<<<<<<<<<<<<< Create Catalogue Service >>>>>>>>>>>>>>>"
+   echo "<<<<<<<<<<<<<<<< Create Catalogue Service >>>>>>>>>>>>>>>"
 cp catalogue.service /etc/systemd/system/catalogue.service
-   echo"<<<<<<<<<<<<<<<< Create Mongodb Repo >>>>>>>>>>>>>>>"
+   echo "<<<<<<<<<<<<<<<< Create Mongodb Repo >>>>>>>>>>>>>>>"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-   echo"<<<<<<<<<<<<<<<< Install nodejs Repos >>>>>>>>>>>>>>>"
+   echo "<<<<<<<<<<<<<<<< Install nodejs Repos >>>>>>>>>>>>>>>"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-   echo"<<<<<<<<<<<<<<<< Install NodeJS >>>>>>>>>>>>>>>"
+   echo "<<<<<<<<<<<<<<<< Install NodeJS >>>>>>>>>>>>>>>"
 yum install nodejs -y
    echo "<<<<<<<<<<<<<<<< Create Application User >>>>>>>>>>>>>>>"
 useradd roboshop
-   echo"<<<<<<<<<<<<<<<< Create Application Directory >>>>>>>>>>>>>>>"
+   echo "<<<<<<<<<<<<<<<< Create Application Directory >>>>>>>>>>>>>>>"
 mkdir /app
-   echo"<<<<<<<<<<<<<<<< Download Application Content >>>>>>>>>>>>>>>"
+   echo "<<<<<<<<<<<<<<<< Download Application Content >>>>>>>>>>>>>>>"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-    echo"<<<<<<<<<<<<<<<< Extraction Application Content >>>>>>>>>>>>>>>"
+    echo "<<<<<<<<<<<<<<<< Extraction Application Content >>>>>>>>>>>>>>>"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
-    echo"<<<<<<<<<<<<<<<< Download nodeJS dependencies >>>>>>>>>>>>>>>"
+    echo "<<<<<<<<<<<<<<<< Download nodeJS dependencies >>>>>>>>>>>>>>>"
 npm install
-    echo"<<<<<<<<<<<<<<<<Install Mongo client >>>>>>>>>>>>>>>"
+    echo "<<<<<<<<<<<<<<<<Install Mongo client >>>>>>>>>>>>>>>"
 yum install mongodb-org-shell -y
-    echo"<<<<<<<<<<<<<<<< Load Catalogue schema >>>>>>>>>>>>>>>"
+    echo "<<<<<<<<<<<<<<<< Load Catalogue schema >>>>>>>>>>>>>>>"
 mongo --host mongodb.nkdevops74.online </app/schema/catalogue.js
 
-    echo"<<<<<<<<<<<<<<<< Start Catalogue service >>>>>>>>>>>>>>>"
+    echo "<<<<<<<<<<<<<<<< Start Catalogue service >>>>>>>>>>>>>>>"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
