bfb766cefb21454e8ee9baa4c490cc57d8485e98
Naresh kumar <naresh.kesa19@gmail.com>
2023-08-23 08:53:51 -0700

    id roboshop added

--- common.sh
+++ common.sh
@@ -1,66 +1,83 @@
 log=/tmp/roboshop.log
+func_exit_status() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+  fi
+}
 func_apppreq()  {
      echo -e "\e[36m<<<<<<<<<<<<<<<< Create User Service >>>>>>>>>>>>>>\e[0m"
     cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
-    echo $?
+    func_exit_status
        echo -e "\e[36m<<<<<<<<<<<<<<<< Create Application User >>>>>>>>>>>>>>\e[0m"
-    useradd roboshop &>>${log}
-    echo $?
+     id roboshop &>>${log}
+      if [ $? -ne 0 ]; then
+        useradd roboshop &>>${log}
+      fi
+    func_exit_status
        echo -e "\e[36m<<<<<<<<<<<<<<<< Cleaning the content >>>>>>>>>>>>>>\e[0m"
     rm -rf /app &>>${log}
-    echo $?
+   func_exit_status
        echo -e "\e[36m<<<<<<<<<<<<<<<< Create Application Directory >>>>>>>>>>>>>>\e[0m"
     mkdir /app &>>${log}
-    echo $?
+    func_exit_status
        echo -e "\e[36m<<<<<<<<<<<<<<<< Download Application Content >>>>>>>>>>>>>>\e[0m"
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-    echo $?
+    func_exit_status
         echo -e "\e[36m<<<<<<<<<<<<<<<< Extraction Application Content >>>>>>>>>>>>>>\e[0m"
     cd /app
     unzip /tmp/${component}.zip &>>${log}
-    echo $?
+    func_exit_status
 }
 func_systemd() {
         echo -e "\e[36m<<<<<<<<<<<<<<<< Start ${component} service >>>>>>>>>>>>>>\e[0m"
     systemctl daemon-reload &>>${log}
     systemctl enable ${component} &>>${log}
     systemctl restart ${component} &>>${log}
-    echo $?
+    func_exit_status
 }
 func_schema_setup() {
   if [ "${schema_type}" == "mongodb" ]; then
-      echo -e "\e[36m<<<<<<<<<<<<<<<<Install Mongo client >>>>>>>>>>>>>>\e[0m"
+    echo -e "\e[36m>>>>>>>>>>>>  Install Mongo Client  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
     yum install mongodb-org-shell -y &>>${log}
-        echo -e "\e[36m<<<<<<<<<<<<<<<< Load Catalogue schema >>>>>>>>>>>>>>\e[0m"
-    mongo --host mongodb.nkdevops74.online </app/schema/${component}.js &>>${log}
-    echo $?
+    func_exit_status
+
+    echo -e "\e[36m>>>>>>>>>>>>  Load User Schema  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+    mongo --host mongodb.rdevopsb72.online </app/schema/${component}.js &>>${log}
+    func_exit_status
   fi
+
   if [ "${schema_type}" == "mysql" ]; then
-   echo -e "\e[36m<<<<<<<<<<<<<<<<Install mysql client >>>>>>>>>>>>>>\e[0m"
+    echo -e "\e[36m>>>>>>>>>>>>  Install MySQL Client   <<<<<<<<<<<<\e[0m"
     yum install mysql -y &>>${log}
-    echo -e "\e[36m<<<<<<<<<<<<<<<<load schema >>>>>>>>>>>>>>\e[0m"
-    mysql -h mysql.nkdevops74.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
-    echo $?
+    func_exit_status
+
+    echo -e "\e[36m>>>>>>>>>>>>  Load Schema   <<<<<<<<<<<<\e[0m"
+    mysql -h mysql.rdevopsb72.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+    func_exit_status
   fi
+
 }
 nodejs() {
   log=/tmp/roboshop.log
 
      echo -e "\e[36m<<<<<<<<<<<<<<<< Create Mongodb Repo >>>>>>>>>>>>>>\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repoec &>>${log}
-     echo $?
+   func_exit_status
+
      echo -e "\e[36m<<<<<<<<<<<<<<<< Install nodejs Repos >>>>>>>>>>>>>>\e[0m"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
-     echo $?
+     func_exit_status
      echo -e "\e[36m<<<<<<<<<<<<<<<< Install NodeJS >>>>>>>>>>>>>>\e[0m"
   yum install nodejs -y &>>${log}
-     echo $?
+    func_exit_status
 
   func_apppreq
 
       echo -e "\e[36m<<<<<<<<<<<<<<<< Download nodeJS dependencies >>>>>>>>>>>>>>\e[0m"
   npm install &>>${log}
-  echo $?
+  func_exit_status
   func_schema_setup
 
   func_systemd
@@ -70,14 +87,14 @@ function_java() {
 
   echo -e "\e[36m<<<<<<<<<<<<<<<<Install Maven >>>>>>>>>>>>>>\e[0m"
   yum install maven -y &>>${log}
-  echo $?
+  func_exit_status
 
   func_apppreq
   echo -e "\e[36m<<<<<<<<<<<<<<<<Build ${component} Service >>>>>>>>>>>>>>\e[0m"
   mvn clean package &>>${log}
-  echo $?
+  func_exit_status
   mv target/${component}-1.0.jar ${component}.jar &>>${log}
-  echo $?
+  func_exit_status
   func_schema_setup
 
   func_systemd
@@ -85,12 +102,12 @@ function_java() {
 func_python () {
   echo -e "\e[36m<<<<<<<<<<<<<<<<Build ${component} Service >>>>>>>>>>>>>>\e[0m"
   yum install python36 gcc python3-devel -y &>>${log}
-  echo $?
+  func_exit_status
 
 
   func_apppreq
   echo -e "\e[36m<<<<<<<<<<<<<<<<Build ${component} Service >>>>>>>>>>>>>>\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
-  echo $?
+ func_exit_status
   func_systemd
 }
