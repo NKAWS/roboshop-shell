a55755994dce6e57fa55a5c79b5d4f87c971919b
Naresh kumar <naresh.kesa19@gmail.com>
2023-08-22 22:32:59 -0700

    Shell Scripting added

--- catalogue.sh
+++ catalogue.sh
@@ -1,19 +1,29 @@
+   echo"<<<<<<<<<<<<<<<<Create Catalogue Service >>>>>>>>>>>>>>>"
 cp catalogue.service /etc/systemd/system/catalogue.service
+   echo"<<<<<<<<<<<<<<<<Create Mongodb Repo >>>>>>>>>>>>>>>"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+   echo"<<<<<<<<<<<<<<<<Install nodejs Repos >>>>>>>>>>>>>>>"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+   echo"<<<<<<<<<<<<<<<<Install NodeJS >>>>>>>>>>>>>>>"
 yum install nodejs -y
+   echo "<<<<<<<<<<<<<<<<Create Application User >>>>>>>>>>>>>>>"
 useradd roboshop
+   echo"<<<<<<<<<<<<<<<<Create Application Directory >>>>>>>>>>>>>>>"
 mkdir /app
+   echo"<<<<<<<<<<<<<<<<Download Application Content >>>>>>>>>>>>>>>"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+    echo"<<<<<<<<<<<<<<<<Extraction Application Content >>>>>>>>>>>>>>>"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
+    echo"<<<<<<<<<<<<<<<<Download nodeJS dependencies >>>>>>>>>>>>>>>"
 npm install
-
+    echo"<<<<<<<<<<<<<<<<Install Mongo client >>>>>>>>>>>>>>>"
 yum install mongodb-org-shell -y
+    echo"<<<<<<<<<<<<<<<<Load Catalogue schema >>>>>>>>>>>>>>>"
 mongo --host mongodb.nkdevops74.online </app/schema/catalogue.js
 
-
+    echo"<<<<<<<<<<<<<<<<Start Catalogue service >>>>>>>>>>>>>>>"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
