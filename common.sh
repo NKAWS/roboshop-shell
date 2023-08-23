e279aa825c7d08e90b178b15a460881f6bf2890d
Naresh kumar <naresh.kesa19@gmail.com>
2023-08-23 05:17:03 -0700

    payment.sh updated

--- common.sh
+++ common.sh
@@ -1,5 +1,7 @@
 log=/tmp/roboshop.log
 func_apppreq()  {
+     echo -e "\e[36m<<<<<<<<<<<<<<<< Create User Service >>>>>>>>>>>>>>\e[0m"
+    cp catalogue.service /etc/systemd/system/${component}.service &>>${log}
        echo -e "\e[36m<<<<<<<<<<<<<<<< Create Application User >>>>>>>>>>>>>>\e[0m"
     useradd roboshop &>>${log}
        echo -e "\e[36m<<<<<<<<<<<<<<<< Cleaning the content >>>>>>>>>>>>>>\e[0m"
@@ -21,8 +23,7 @@ func_systemd() {
 }
 nodejs() {
   log=/tmp/roboshop.log
-     echo -e "\e[36m<<<<<<<<<<<<<<<< Create User Service >>>>>>>>>>>>>>\e[0m"
-  cp catalogue.service /etc/systemd/system/${component}.service &>>${log}
+
      echo -e "\e[36m<<<<<<<<<<<<<<<< Create Mongodb Repo >>>>>>>>>>>>>>\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repoec &>>${log}
      echo -e "\e[36m<<<<<<<<<<<<<<<< Install nodejs Repos >>>>>>>>>>>>>>\e[0m"
@@ -53,15 +54,21 @@ function_java() {
   mvn clean package &>>${log}
   mv target/${component}-1.0.jar ${component}.jar &>>${log}
 
-
   echo -e "\e[36m<<<<<<<<<<<<<<<<Install mysql client >>>>>>>>>>>>>>\e[0m"
   yum install mysql -y &>>${log}
   echo -e "\e[36m<<<<<<<<<<<<<<<<load schema >>>>>>>>>>>>>>\e[0m"
   mysql -h mysql.nkdevops74.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
 
-
-
   func_systemd
+}
+func_python () {
+  echo -e "\e[36m<<<<<<<<<<<<<<<<Build ${component} Service >>>>>>>>>>>>>>\e[0m"
+  yum install python36 gcc python3-devel -y &>>${log}
 
+  useradd roboshop
 
+  func_apppreq
+  echo -e "\e[36m<<<<<<<<<<<<<<<<Build ${component} Service >>>>>>>>>>>>>>\e[0m"
+  pip3.6 install -r requirements.txt &>>${log}
+  func_systemd
 }
